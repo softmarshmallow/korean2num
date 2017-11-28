42443947f17dadaec9bfce8c268286ccb4572a49
Junseong Kim <codertimo@gmail.com>
2017-11-28 21:26:19 +0900

    Koran to Num

---
+++ korean2num.py
@@ -0,0 +1,92 @@
+import math
+
+"""
+Developed by Junseong Kim, Atlas Guide
+codertimo@goodatlas.com / github.com/codertimo
+Korean to number
+"""
+
+numbers = [
+    ("스물", 20),
+    ("서른", 30),
+    ("마흔", 40),
+    ("쉰", 50),
+    ("예순", 60),
+    ("일흔", 70),
+    ("여든", 80),
+    ("아흔", 90),
+    ("일", 1),
+    ("이", 2),
+    ("삼", 3),
+    ("사", 4),
+    ("오", 5),
+    ("육", 6),
+    ("칠", 7),
+    ("팔", 8),
+    ("구", 9),
+    ("하나", 1),
+    ("한", 1),
+    ("두", 2),
+    ("둘", 2),
+    ("세", 3),
+    ("셋", 3),
+    ("네", 4),
+    ("넷", 4),
+    ("다섯", 5),
+    ("여섯", 6),
+    ("일곱", 7),
+    ("여덟", 8),
+    ("여덜", 8),
+    ("아홉", 9),
+    ("열", 10),
+    ("십", 10),
+    ("백", 100),
+    ("천", 1000),
+    ("만", 10000),
+    ("억", 100000000),
+    ("조", 1000000000000),
+    ("경", 10000000000000000),
+    ("해", 100000000000000000000),
+]
+
+
+def decode(korean_num):
+    decode_result = []
+    result = 0
+    temp_result = 0
+    index = 0
+
+    while index < len(korean_num):
+        for number, true_value in numbers:
+            if index + len(number) <= len(korean_num):
+                if korean_num[index:index + len(number)] == number:
+                    decode_result.append((true_value, math.log10(true_value).is_integer()))
+                    if len(number) == 2:
+                        index += 1
+                    break
+        index += 1
+
+    for index, (number, is_natural) in enumerate(decode_result):
+        if is_natural:
+            if math.log10(number) > 3 and (math.log10(number) - 4) % 4 == 0:
+                result += temp_result * number
+                temp_result = 0
+
+            elif index - 1 >= 0:
+                if not decode_result[index - 1][1]:
+                    temp_result += number * decode_result[index - 1][0]
+                else:
+                    temp_result += number
+            else:
+                temp_result += number
+
+        else:
+            if index + 1 == len(decode_result):
+                temp_result += number
+            elif not decode_result[index + 1][1]:
+                temp_result += number
+            elif math.log10(decode_result[index + 1][0]) > 3 and (math.log10(decode_result[index + 1][0]) - 4) % 4 == 0:
+                temp_result += number
+
+    result += temp_result
+    return result
